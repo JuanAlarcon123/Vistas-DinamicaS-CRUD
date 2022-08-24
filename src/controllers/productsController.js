1592cd75bb5478855695af8579128f3ad201d567
JuanAlarcon123 <grabados.todoenvidrios@gmail.com>
2022-08-24 12:43:44 -0300

    edicion de producto en proceso

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -13,6 +13,7 @@ const controller = {
 	},
 
 	// Detail - Detail from one product
+	
 	detail: (req, res) => {
 		let product = products.find(product => product.id === +req.params.id)// aca filtro los productos i se hace visible el producto que sea ifual al id
 	
@@ -35,12 +36,32 @@ const controller = {
 
 	// Update - Form to edit
 	edit: (req, res) => {
+		const product = products.find ( product => product.id === +req.params.id)
+		res.render('product-edit-form', {product
+		})
 		// Do the magic
 	},
 	// Update - Method to update
 	update: (req, res) => {
+		const {name,description,price,discount,category} = req.body
+		const productModify = products.map(product => { 
+			if(product.id === +req.params.id) {
+				return {
+					...product,
+					name: name.trim(),
+					price : +price,
+					discount: +discount,
+					category,
+					description:description.trim()
+				}}	
+				return product
+		})
+
+		fs.writeFileSync(path.join(__dirname,'..','data','productsDataBase.json'), JSON.stringify(productModify),'utf-8')
+		res.redirect('/')
+
 		// Do the magic
-	},
+	} ,
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
